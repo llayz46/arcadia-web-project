18844ee1c2864358e1f1379eb3cb4f4420b90fc6
llayz46 <layzer@outlook.fr>
2024-05-15 17:48:27 +0200

    add: config, pdo, mongodb

---
+++ lib/config.php
@@ -0,0 +1,18 @@
+<?php
+
+define('_DB_HOST_', getenv('DB_HOST'));
+define('_DB_NAME_', getenv('DB_NAME'));
+define('_DB_PORT_', getenv('DB_PORT'));
+define('_DB_USER_', getenv('DB_USER'));
+define('_DB_PASS_', getenv('DB_PASS'));
+
+define('_MONGO_URL_', getenv('MONGO_URL'));
+define('_MONGO_DB_', getenv('MONGO_DB'));
+define('_MONGO_COLLECTION_', getenv('MONGO_COLLECTION'));
+
+define('_CONTACT_MAIL_', getenv('CONTACT_MAIL'));
+
+define('_PATH_ASSETS_IMAGES_', 'assets/images/');
+define('_PATH_UPLOADS_', 'assets/uploads/');
+
+define('_ALLOWED_EXTENSIONS_', ['jpg', 'jpeg', 'png', 'gif']);
---
+++ lib/mongodb.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+try {
+  $mongodb = new MongoDB\Client(_MONGO_URL_);
+  $database = $mongodb->selectDatabase(_MONGO_DB_);
+  $collection = $database->selectCollection(_MONGO_COLLECTION_);
+} catch (Exception $e) {
+  die('Erreur de connexion à MongoDB : ' . $e->getMessage());
+}
