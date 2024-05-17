ecebfb8f5386a11f8c74bdd366470fc10700fd91
llayz46 <layzer@outlook.fr>
2024-05-17 15:41:37 +0200

    remvoe: sass from index git

--- lib/mongodb.php
+++ lib/mongodb.php
@@ -2,10 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Exception;
+use MongoDB\Client;
+
 try {
   $mongodb = new MongoDB\Client(_MONGO_URL_);
-  $database = $mongodb->selectDatabase(_MONGO_DB_);
-  $collection = $database->selectCollection(_MONGO_COLLECTION_);
+  $database = $mongodb->selectDatabase(_MONGO_DB_)->command(['ping' => 1]);
+  $collection = $database->selectCollection(_MONGO_COLLECTION_)->command(['ping' => 1]);
 } catch (Exception $e) {
   die('Erreur de connexion à MongoDB : ' . $e->getMessage());
 }
