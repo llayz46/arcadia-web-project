8d2fb9421106ff71903d652d608c282b872e8927
llayz46 <layzer@outlook.fr>
2024-05-20 12:46:43 +0200

    fix: config.php

--- lib/config.php
+++ lib/config.php
@@ -1,9 +1,8 @@
 <?php
 
-define('_DOMAIN_', 'your_domain');
-define('_DB_HOST_', 'your_db_host');
+define('_DOMAIN_', 'myproject.local');
+define('_DB_HOST_', 'localhost');
 define('_DB_NAME_', 'your_db_name');
-define('_DB_PORT_', 'your_db_port');
 define('_DB_USER_', 'your_db_user');
 define('_DB_PASS_', 'your_db_pass');
 
--- lib/pdo.php
+++ lib/pdo.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-  $pdo = new PDO('mysql:host='._DB_HOST_.';port='._DB_PORT_.';dbname='._DB_NAME_.';charset=utf8mb4', _DB_USER_, _DB_PASS_, [
+  $pdo = new PDO('mysql:host='._DB_HOST_.';dbname='._DB_NAME_.';charset=utf8mb4', _DB_USER_, _DB_PASS_, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
   ]);
