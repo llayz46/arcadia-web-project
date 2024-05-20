30855b554e47aab59d966582047ddd7f31e30da0
llayz46 <layzer@outlook.fr>
2024-05-20 16:53:32 +0200

    fix on service page

--- habitat.php
+++ habitat.php
@@ -80,6 +80,22 @@ require_once __DIR__ . '/templates/header.php';
               </button>
             </li>
           <?php } ?>
+          <li class="habitats__img-item">
+            <div class="habitats__img-images js-habitats-images"></div>
+            <button class="habitats__img-icon-wrapper js-habitats-icon">
+              <svg class="habitats__img-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                <path d="M13.4686 6.49175H7.53135V0.531353C7.53135 0.254125 7.30033 0 7 0C6.72277 0 6.46865 0.231023 6.46865 0.531353V6.49175H0.531353C0.254125 6.49175 0 6.72277 0 7.0231C0 7.30033 0.231023 7.55446 0.531353 7.55446H6.49175V13.4686C6.49175 13.7459 6.72277 14 7.0231 14C7.30033 14 7.55446 13.769 7.55446 13.4686V7.53135H13.4686C13.7459 7.53135 14 7.30033 14 7C14 6.72277 13.7459 6.49175 13.4686 6.49175Z" fill="white" />
+              </svg>
+            </button>
+          </li>
+          <li class="habitats__img-item">
+            <div class="habitats__img-images js-habitats-images"></div>
+            <button class="habitats__img-icon-wrapper js-habitats-icon">
+              <svg class="habitats__img-icon" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+                <path d="M13.4686 6.49175H7.53135V0.531353C7.53135 0.254125 7.30033 0 7 0C6.72277 0 6.46865 0.231023 6.46865 0.531353V6.49175H0.531353C0.254125 6.49175 0 6.72277 0 7.0231C0 7.30033 0.231023 7.55446 0.531353 7.55446H6.49175V13.4686C6.49175 13.7459 6.72277 14 7.0231 14C7.30033 14 7.55446 13.769 7.55446 13.4686V7.53135H13.4686C13.7459 7.53135 14 7.30033 14 7C14 6.72277 13.7459 6.49175 13.4686 6.49175Z" fill="white" />
+              </svg>
+            </button>
+          </li>
         </ul>
       </nav>
     </div>
